9b9f408fcf5464fc58624711a98f16934cdecddd
Neiron <hitman2491@gmail.com>
2019-04-08 13:48:34 +0700

    Print current vm.max_map_count from check dependencies

--- src/models/CheckDependencies.php
+++ src/models/CheckDependencies.php
@@ -660,6 +660,7 @@ apt-get install wine32 wine binutils unzip cabextract p7zip-full unrar-free wget
         if ($this->system->getVmMaxMapCount() < 200000) {
             $this->log('');
             $this->log('Please set vm.max_map_count=262144');
+            $this->log('Current: ' . $this->system->getVmMaxMapCount());
             $this->log('Run as root:');
             $this->log('echo \'vm.max_map_count=262144\' >> /etc/sysctl.conf');
         }
